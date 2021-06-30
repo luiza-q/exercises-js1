9b7eceb3cfbbd4318a279a9fc67f09e1189110c2
Luiza Quaglio <luizaq@gmail.com>
2021-06-30 19:22:29 +0200

    homework week 1 done

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -1,7 +1,15 @@
 function halve(number) {
-  // complete the function here
+  return number / 2;
 }
 
 var result = halve(12);
+
+console.log(result);
+
+var result = halve(20);
+
+console.log(result);
+
+var result = halve(8);
 
 console.log(result);
--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -3,3 +3,20 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function nameUpper(name) {
+    return name.toUpperCase();
+
+} 
+
+function greetingMentors(name) {
+    const message = "HELLO " + nameUpper(name);
+    return message;
+}
+
+
+console.log(greetingMentors(mentor1));
+console.log(greetingMentors(mentor2));
+console.log(greetingMentors(mentor3));
+console.log(greetingMentors(mentor4));
+console.log(greetingMentors(mentor5));
